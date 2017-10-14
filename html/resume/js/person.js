15c740ee0f4a4126b52d5d43394c99edf8cc4618
yanxxmh <yanxiaoxiao@mallog.cn>
2017-10-14 18:17:01 +0800

    update

---
+++ html/resume/js/person.js
@@ -0,0 +1,15 @@
+//个人信息
+var person = {
+    name: '闫肖肖',
+    sex: '男',
+    age: '27',
+    mobile: '15806111230',
+    love: '编程与跑步',
+    headimg: '头像地址',
+    work_year:'5',
+    good:{
+        lang:'node.js golang ',
+
+    },//擅长
+
+};
